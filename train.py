e0d2487577b3f4ab05ebea81ee2ae66927832019
Sage <hello@sageelliott.com>
2020-04-22 21:39:14 -0700

    update: better comments on train.py

--- train.py
+++ train.py
@@ -1,8 +1,3 @@
-#%%
-##########################
-# Imports 
-##########################
-
 # import system libraries
 import sys
 import os
@@ -65,42 +60,47 @@ def load_train(verbose, device, num_classes, n_epochs, learn_rate, save_path,
 
     TODO: 
         - run folder check and create
+        - better error handling if no model
     '''
     # if data labels are to be loaded from a CSV file
     if csv_labels:
-        # Labels from CSV
+        # CSV to DataFrame
         train_df = pd.read_csv(csv_labels)
 
-        # One hot encoding
+        # One hot encoding for csv labels
         train_df.Label = pd.Categorical(pd.factorize(train_df.Label)[0])
 
+        # if validation dataset included use it
+        # else perfrom train / val split on training data
         if val_data:
-            # Labels from CSV
+            # CSV to DataFrame
             val_df = pd.read_csv(val_data)
-
-            # One hot encoding
+            # One hot encoding for csv labels
             val_df.Label = pd.Categorical(pd.factorize(val_df.Label)[0])
-            
         else:
             # Create Train & Validation split
             train_df, val_df = val_train_split(train_df, 0.2)
 
-        # Create Data loaders from CSV data
+        # Create train & validation data loaders from CSV data
         train_loader = csv_loader_stack(data_dir,train_df, 'FilePath', 'Label',
                                 img_size,batch_size,num_workers,True)
 
         val_loader = csv_loader_stack(data_dir,val_df, 'FilePath', 'Label',
                                 img_size,batch_size,num_workers,False)
     
-    # If data labels are to be loaded from directories
+    # else load data from directory if no CSV labels given
     else:
+        # if validation dataset included use it
+        # else perfrom train / val split on training data
         if val_data:
+            # Create train & validation data loaders from directory
             train_loader =  dir_loader_stack(data_dir, img_size, batch_size, 
                                                     num_workers, True)
             
             val_loader = dir_loader_stack(val_data, img_size, batch_size, 
                                                         num_workers,False)
         else:
+            # Create Train & Validation split from data loader
             loader = dir_loader_stack(data_dir, img_size, batch_size,
                                         num_workers, True)
             train_size = int(0.8 * len(loader.dataset))
@@ -108,69 +108,63 @@ def load_train(verbose, device, num_classes, n_epochs, learn_rate, save_path,
             train_data, val_data = torch.utils.data.random_split(loader.dataset,
                                                             [train_size, 
                                                             test_size])
-
+            # Create train & validation data loaders from directory
             train_loader = torch.utils.data.DataLoader(train_data,
                                                         batch_size=batch_size)
             val_loader = torch.utils.data.DataLoader(val_data, 
                                                         batch_size=batch_size)
-
+    # Create loaders object to pass for training
     loaders = {
         'train':train_loader,
         'valid':val_loader
     }
+    # Print train & validation lengths
     print('Training length: ' + str(len(train_loader.dataset)))
     print('Validation length: ' + str(len(val_loader.dataset)))
   
-    # verify images before training
-    if verbose: 
-        # Train Data sample
+    
+    if verbose:
+        # Output training and validation images before training
         print("\nTraining images")
         image_plot(train_loader)
-
-        # validation data sample
         print("\nValidation images")
         image_plot(val_loader)
 
-    # create model from model class
-
-    #resnet50 mode
+    # Load model, Set criterion & set optimizer based on args
+    # see README.md for more details on model
+    # see /models folder for model classes
     if model_type == 'resnet50':
         model = Resnet50_pretrained(num_classes)
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.SGD(model.model.fc.parameters(), lr=learn_rate)
         train_model = model.model
-    
-    # vgg model
     elif model_type == 'vgg16':
         model = vgg16_pretrained(num_classes)
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.SGD(model.model.classifier._modules['6'].parameters(),
                                                                 lr=learn_rate)
         train_model = model.model
-    
     elif model_type == 'scratch':
         model = Scratch_net(num_classes)
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.SGD(model.classifier._modules['6'].parameters(),
                                                                 lr=learn_rate)
         train_model = model
-
     else:
         print('\n!---------------------------------!\n')
         print('please select an existing model type or create you own')
         print('\n!---------------------------------!\n')
     
-
-    # print model to terminal
     if verbose:
+        # print model architecture to terminal
         print(train_model)
 
-    # Train 
+    # Train model based on input args
     H = train(train_model, n_epochs, loaders, optimizer,
                         criterion, device, save_path)
  
     if verbose:
-        # Train Log
+        # Output Training Log (Train & Val: acc & loss per epoch)
         plot_train_history(H,n_epochs)
 
 if __name__ == '__main__':
