c71d53610c0305c07fe1c5fc7e4ae6b709221fd8
Sage <hello@sageelliott.com>
2020-04-28 20:54:28 -0700

    update: remove VS code folder from git

--- app.py
+++ app.py
@@ -42,11 +42,8 @@ def hello():
 @app.route('/predict', methods=['GET', 'POST'])
 def prediction():
     if request.method == 'POST':
-        # we will get the file from the request
+        # get file from the request
         file = request.files['file']
-        # convert that to bytes
-        # img_bytes = file.read()
-        # class_id, class_name = get_prediction(image_bytes=img_bytes)
         prediction = predict(res_model.model, file, device, True)
         return jsonify({'class_id': prediction})
         
